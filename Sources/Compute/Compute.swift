9438bcf0b300fd6525312109fc24ec6926afd690
Andrew Barba <barba@hey.com>
2022-02-10 12:03:28 -0500

    Add compute status request

--- Sources/Compute/Compute.swift
+++ Sources/Compute/Compute.swift
@@ -9,9 +9,25 @@ public func onIncomingRequest(_ handler: @escaping (_ req: IncomingRequest, _ re
     let req = try IncomingRequest()
     let res = try OutgoingResponse()
     do {
+        if isComputeStatusRequest(req) {
+            return try await sendComputeStatusResponse(res)
+        }
         try await handler(req, res)
     } catch {
         print("onIncomingRequest:error", error.localizedDescription)
         try await res.status(500).send("Server error: \(error.localizedDescription)")
     }
+}
+
+private func isComputeStatusRequest(_ req: IncomingRequest) -> Bool {
+    return req.method == .get && req.url.path == "/__compute-status"
+}
+
+private func sendComputeStatusResponse(_ res: OutgoingResponse) async throws {
+    try await res
+        .status(204)
+        .header("x-compute-service-id", Environment.Compute.serviceId)
+        .header("x-compute-service-version", Environment.Compute.serviceVersion)
+        .header("x-compute-trace-id", Environment.Compute.traceId)
+        .send()
 }
