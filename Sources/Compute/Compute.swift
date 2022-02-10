054a9a66d974bd809deb43472368f1c254eb5c3f
Andrew Barba <barba@hey.com>
2022-02-10 17:45:54 -0500

    Comments

--- Sources/Compute/Compute.swift
+++ Sources/Compute/Compute.swift
@@ -6,15 +6,25 @@
 //
 
 public func onIncomingRequest(_ handler: @escaping (_ req: IncomingRequest, _ res: OutgoingResponse) async throws -> Void) async throws {
+    // Initialize ABI
     try ABI.initialize()
+
+    // Get downstream request
     let req = try IncomingRequest()
+
+    // Create downstream response
     let res = try OutgoingResponse()
+
     do {
+        // Check for special compute request, useful for pre-warming
         if isComputeStatusRequest(req) {
             return try await sendComputeStatusResponse(res)
         }
+
+        // Run main handler
         try await handler(req, res)
     } catch {
+        // Catch handler error by returning a 500
         print("onIncomingRequest:error", error.localizedDescription)
         try await res.status(500).send("Server error: \(error.localizedDescription)")
     }
