4a75e71f4ff19a9f72e2c9724f28db3c8f8b7622
Andrew Barba <barba@hey.com>
2022-03-31 13:35:01 -0400

    Sync local store, async object store

--- Sources/Compute/LocalStore.swift
+++ Sources/Compute/LocalStore.swift
@@ -5,6 +5,61 @@
 //  Created by Andrew Barba on 3/30/22.
 //
 
-public func LocalStore() throws -> ObjectStore {
-    return try .init(name: localStoreName)
+import Foundation
+
+public struct LocalStore: Sendable {
+
+    internal let store: Store
+
+    public init() throws {
+        store = try .init(name: localStoreName)
+    }
+
+    public func lookup(_ key: String) throws -> ReadableBody? {
+        guard let body = try store.lookup(key) else {
+            return nil
+        }
+        return .init(body)
+    }
+
+    public func has(_ key: String) throws -> Bool {
+        switch try lookup(key) {
+        case .some:
+            return true
+        case .none:
+            return false
+        }
+    }
+
+    public func insert(_ key: String, body: ReadableBody, maxAge: Int = 0) async throws {
+        try await store.insert(key, body: body.body, maxAge: maxAge)
+    }
+
+    public func insert(_ key: String, bytes: [UInt8], maxAge: Int = 0) throws {
+        try store.insert(key, bytes: bytes, maxAge: maxAge)
+    }
+
+    public func insert(_ key: String, data: Data, maxAge: Int = 0) throws {
+        try insert(key, bytes: .init(data), maxAge: maxAge)
+    }
+
+    public func insert(_ key: String, text: String, maxAge: Int = 0) throws {
+        let data = text.data(using: .utf8) ?? .init()
+        try insert(key, data: data, maxAge: maxAge)
+    }
+
+    public func insert(_ key: String, jsonObject: [String: Any], maxAge: Int = 0) throws {
+        let data = try JSONSerialization.data(withJSONObject: jsonObject, options: [])
+        try insert(key, data: data, maxAge: maxAge)
+    }
+
+    public func insert(_ key: String, jsonArray: [Any], maxAge: Int = 0) throws {
+        let data = try JSONSerialization.data(withJSONObject: jsonArray, options: [])
+        try insert(key, data: data, maxAge: maxAge)
+    }
+
+    public func insert<T>(_ key: String, value: T, encoder: JSONEncoder = .init(), maxAge: Int = 0) throws where T: Encodable {
+        let data = try encoder.encode(value)
+        try insert(key, data: data, maxAge: maxAge)
+    }
 }
--- Sources/Compute/ObjectStore.swift
+++ Sources/Compute/ObjectStore.swift
@@ -19,15 +19,15 @@ public struct ObjectStore: Sendable {
         store.name
     }
 
-    public func lookup(_ key: String) throws -> ReadableBody? {
+    public func lookup(_ key: String) async throws -> ReadableBody? {
         guard let body = try store.lookup(key) else {
             return nil
         }
         return .init(body)
     }
 
-    public func has(_ key: String) throws -> Bool {
-        switch try lookup(key) {
+    public func has(_ key: String) async throws -> Bool {
+        switch try await lookup(key) {
         case .some:
             return true
         case .none:
@@ -35,36 +35,36 @@ public struct ObjectStore: Sendable {
         }
     }
 
-    public func insert(_ key: String, body: HttpBody, maxAge: Int = 0) throws {
-        try store.insert(key, body: body, maxAge: maxAge)
+    public func insert(_ key: String, body: ReadableBody, maxAge: Int = 0) async throws {
+        try await store.insert(key, body: body.body, maxAge: maxAge)
     }
 
-    public func insert(_ key: String, bytes: [UInt8], maxAge: Int = 0) throws {
+    public func insert(_ key: String, bytes: [UInt8], maxAge: Int = 0) async throws {
         try store.insert(key, bytes: bytes, maxAge: maxAge)
     }
 
-    public func insert(_ key: String, data: Data, maxAge: Int = 0) throws {
-        return try insert(key, bytes: .init(data), maxAge: maxAge)
+    public func insert(_ key: String, data: Data, maxAge: Int = 0) async throws {
+        try await insert(key, bytes: .init(data), maxAge: maxAge)
     }
 
-    public func insert(_ key: String, text: String, maxAge: Int = 0) throws {
+    public func insert(_ key: String, text: String, maxAge: Int = 0) async throws {
         let data = text.data(using: .utf8) ?? .init()
-        return try insert(key, data: data, maxAge: maxAge)
+        try await insert(key, data: data, maxAge: maxAge)
     }
 
-    public func insert(_ key: String, jsonObject: [String: Any], maxAge: Int = 0) throws {
+    public func insert(_ key: String, jsonObject: [String: Any], maxAge: Int = 0) async throws {
         let data = try JSONSerialization.data(withJSONObject: jsonObject, options: [])
-        try insert(key, data: data, maxAge: maxAge)
+        try await insert(key, data: data, maxAge: maxAge)
     }
 
-    public func insert(_ key: String, jsonArray: [Any], maxAge: Int = 0) throws {
+    public func insert(_ key: String, jsonArray: [Any], maxAge: Int = 0) async throws {
         let data = try JSONSerialization.data(withJSONObject: jsonArray, options: [])
-        try insert(key, data: data, maxAge: maxAge)
+        try await insert(key, data: data, maxAge: maxAge)
     }
 
-    public func insert<T>(_ key: String, value: T, encoder: JSONEncoder = .init(), maxAge: Int = 0) throws where T: Encodable {
+    public func insert<T>(_ key: String, value: T, encoder: JSONEncoder = .init(), maxAge: Int = 0) async throws where T: Encodable {
         let data = try encoder.encode(value)
-        try insert(key, data: data, maxAge: maxAge)
+        try await insert(key, data: data, maxAge: maxAge)
     }
 }
 
