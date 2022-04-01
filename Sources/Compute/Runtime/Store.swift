33ec5a92c34e7c6f420a11d9cf6d2873f204a9d6
Andrew Barba <barba@hey.com>
2022-04-01 17:50:03 -0400

    Merge main

--- Sources/Compute/Runtime/Store.swift
+++ Sources/Compute/Runtime/Store.swift
@@ -20,23 +20,23 @@ public struct Store: Sendable {
         self.name = name
     }
 
-    public func lookup(_ key: String) throws -> HttpBody? {
+    public func lookup(_ key: String) throws -> Body? {
         do {
             var bodyHandle: BodyHandle = 0
             try wasi(fastly_kv__lookup(handle, key, key.utf8.count, &bodyHandle))
-            return HttpBody(bodyHandle)
+            return Body(bodyHandle)
         } catch WasiStatus.none {
             return nil
         }
     }
 
-    public func insert(_ key: String, body: HttpBody, maxAge: Int) throws {
+    public func insert(_ key: String, body: Body, maxAge: Int) throws {
         var inserted: UInt32 = 0
         try wasi(fastly_kv__insert(handle, key, key.utf8.count, body.handle, .init(maxAge), &inserted))
     }
 
     public func insert(_ key: String, bytes: [UInt8], maxAge: Int) throws {
-        var body = try HttpBody()
+        var body = try Body()
         try body.write(bytes)
         try insert(key, body: body, maxAge: maxAge)
     }
