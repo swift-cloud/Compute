1c33ce8bef2852f48ee494db049bb39417526479
Andrew Barba <barba@hey.com>
2022-04-04 23:22:50 -0400

    Add router.all

--- Sources/Compute/Router.swift
+++ Sources/Compute/Router.swift
@@ -30,13 +30,13 @@ public final class Router {
     }
 
     internal func handler(for req: IncomingRequest) -> (handler: Handler, pathComponents: PathParameters)? {
-        guard let _handlers = handlers[req.method.rawValue] else {
+        guard let handlers = handlers[req.method.rawValue] else {
             return nil
         }
 
-        let parts = req.url.path.components(separatedBy: "/").filter { $0.count > 0 }
+        let parts = req.url.path.components(separatedBy: "/").filter { $0.isEmpty == false }
 
-        for handler in _handlers {
+        for handler in handlers {
             if isMatchingPath(internalComponents: handler.paths, externalComponents: parts) {
                 let pathParamaters = parsePathParameters(internalComponents: handler.paths, externalComponents: parts)
                 return (handler.handler, pathParamaters)
@@ -117,6 +117,14 @@ extension Router {
     @discardableResult
     public func head(_ path: String, _ handler: @escaping Handler) -> Self {
         return add(method: .head, path: path, handler: handler)
+    }
+
+    @discardableResult
+    public func all(_ path: String, _ handler: @escaping Handler) -> Self {
+        for method in HTTPMethod.allCases {
+            add(method: method, path: path, handler: handler)
+        }
+        return self
     }
 }
 
