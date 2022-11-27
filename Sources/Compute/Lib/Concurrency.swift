28702ca2ea92adc83852d3ca69de120029f927ba
Andrew Barba <barba@hey.com>
2022-11-27 14:26:07 -0500

    Mark url and data as sendable

---
+++ Sources/Compute/Lib/Concurrency.swift
@@ -0,0 +1,12 @@
+//
+//  Concurrency.swift
+//  
+//
+//  Created by Andrew Barba on 11/27/22.
+//
+
+import Foundation
+
+extension Data: @unchecked Sendable {}
+
+extension URL: @unchecked Sendable {}
