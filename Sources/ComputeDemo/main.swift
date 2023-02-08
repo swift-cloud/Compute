39aa83b544d2d8c21c363872571050ee382a9cd8
Andrew Barba <barba@hey.com>
2023-02-08 10:44:40 -0500

    Update demo

--- Sources/ComputeDemo/main.swift
+++ Sources/ComputeDemo/main.swift
@@ -1,9 +1,16 @@
 import Compute
+import Crypto
+
+private let token =
+    """
+    eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJleHAiOjE2NzUzNjU0MjgsImlzcyI6ImZhc3RseSJ9.QL2Pm1JnXV_vAYK7ijeD4U1CBjOTLihNMDZ-qfvjkKOTUiK1jyxGEwjZfeApijRaOtQT8fVkdPnKjF-tBiUzkA
+    """
 
 try await onIncomingRequest { req, res in
-    for _ in Array(0...1000) {
-        let jwt = try JWT(claims: ["user_id": UUID().uuidString], secret: UUID().uuidString)
-        try await res.write(jwt.token + "\n")
-    }
-    try await res.end()
+    let jwt = try JWT(token: token)
+    let message = token.components(separatedBy: ".").dropLast().joined(separator: ".")
+    let pk = ECDSA.PublicKey(pem: fanoutPublicKey, curve: .secp256r1)
+    let sig = ECDSA.Signature(data: .init(jwt.signature))
+    let verified = pk.verify(message: message, signature: sig)
+    try await res.send(["v": verified])
 }
